1d63bb1b9e2bab9786c45a7615a6fc2aa879d6ba
badr <badrhand@gmail.com>
2022-01-28 15:57:33 +0100

    adding the code to github
    	# Please enter the commit message for your changes. Lines starting

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false,
+  // content: [
+  //   // "./pages/**/*.{js,ts,jsx,tsx}",
+  //   // "./components/*.{js,ts,jsx,tsx}",
+  //   // "./components/*.js",
+  //   // "./pages/*.",
+  // ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+};
