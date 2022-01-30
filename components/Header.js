97103ac5e064c958a6dff8f668359df442880b9f
badr <badrhand@gmail.com>
2022-01-30 12:12:52 +0100

    add this calander part

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,29 @@ import {
   SearchIcon,
   GlobeAltIcon,
   MenuIcon,
+  UsersIcon,
   UserCircleIcon,
 } from "@heroicons/react/solid";
+import { useState } from "react";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
 function Header() {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+
+  const handleSelect = (ranges) => {
+    console.log(ranges)
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       <div className="relative flex-initial items-center h-10 cursor-pointer my-auto">
@@ -22,6 +42,8 @@ function Header() {
         <input
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
           type="text"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           placeholder="start your search"
         />
         <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
@@ -34,6 +56,21 @@ function Header() {
           <UserCircleIcon className="h-6" />
         </div>
       </div>
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto mt-10">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
+          />
+          <div>
+            <h2 className="text-xl flex-grow font-semibold">Number of Guests</h2>
+
+            <UsersIcon className="h-5"/>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
